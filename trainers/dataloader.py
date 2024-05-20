1e6a049f7e87a6ae1ba8ede66e36468897f8271e
bobbycxy <bobbycxy1994@gmail.com>
2024-05-20 16:46:37 +0800

    + adding MLM toggle that generates inputs and outputs for MLM

--- trainers/dataloader.py
+++ trainers/dataloader.py
@@ -3,17 +3,20 @@ Create a data loader for the model
 '''
 
 import torch
-from tokenizer import character, simplebpe
+from tokenizer import character, simplebpe, characternew
+from trainers.utils import masked_lm
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 tokenizer_dict = {
     'character': character,
-    'simplebpe': simplebpe
+    'simplebpe': simplebpe,
+    'characternew': characternew
 }
 
 class DataLoader:
     def __init__(self, cfg):
+        self.cfg = cfg
         with open(cfg['train_file'],'r') as f:
             self.text = f.read()
         self.block_size = cfg['block_size']
@@ -29,6 +32,12 @@ class DataLoader:
         data = self.train_data if split == 'train' else self.val_data
         idx = torch.randint(0, len(data) - self.block_size, (self.batch_size,))
         x = torch.stack([data[i     :   i+self.block_size   ] for i in idx])
-        y = torch.stack([data[i+1   :   i+self.block_size+1 ] for i in idx])
+        
+        ## apply mlm if masked language model is true
+        if self.cfg['masked_lm']: 
+            x, y = masked_lm(x, self.tokenizer, self.cfg['mlm_prob'])
+        else: ## else, shift the data by one
+            y = torch.stack([data[i+1   :   i+self.block_size+1 ] for i in idx])
+        
         return x.to(device), y.to(device)
         
