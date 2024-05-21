2750efadb2e55fc929642b4dc9327b8f68f1747a
bobbycxy <bobbycxy1994@gmail.com>
2024-05-21 11:41:33 +0800

    + test data set for using

--- trainers/dataloader.py
+++ trainers/dataloader.py
@@ -26,16 +26,25 @@ class DataLoader:
         self.data = torch.tensor(self.tokenizer.encode(self.text), dtype=torch.long)
 
         n = int(cfg['train_size']*len(self.data))
-        self.train_data, self.val_data = self.data[:n], self.data[n:]
+        train_val = int(0.9*len(self.data)) ## 90% train and val, 10% test
+        self.train_data, self.val_data, self.test_data = self.data[:n*train_val], self.data[n*train_val:train_val], self.data[train_val:]
 
     def load(self, split):
-        data = self.train_data if split == 'train' else self.val_data
+        data_dict = {
+            'train': self.train_data,
+            'val': self.val_data,
+            'test': self.test_data
+        }
+        data = data_dict[split]
         idx = torch.randint(0, len(data) - self.block_size, (self.batch_size,))
         x = torch.stack([data[i     :   i+self.block_size   ] for i in idx])
+
+        if split == 'test':
+            return x.to(device), None
         
         ## apply mlm if masked language model is true
-        if self.cfg['masked_lm']: 
-            x, y = masked_lm(x, self.tokenizer, self.cfg['mlm_prob'])
+        if self.cfg['languagemodel']['masked_lm']: 
+            x, y = masked_lm(x, self.tokenizer, self.cfg['languagemodel']['mlm_prob'])
         else: ## else, shift the data by one
             y = torch.stack([data[i+1   :   i+self.block_size+1 ] for i in idx])
         
